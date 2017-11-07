9cb8082228a0981387277b10bd87dd2040c43a2b
James Connor <megakid@gmail.com>
2017-11-07 23:43:19 +0000

    Start

---
+++ Src/Adapters/AspNetCore.Metrics/AspNetCoreMetricsConfig.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using AspNetCore.Metrics.Middleware;
+using Metrics;
+using Metrics.Reports;
+using Microsoft.AspNetCore.Http;
+
+namespace AspNetCore.Metrics
+{
+    public class AspNetCoreMetricsConfig
+    {
+        public static readonly AspNetCoreMetricsConfig Disabled = new AspNetCoreMetricsConfig();
+
+        private readonly Action<Func<HttpContext, Func<Task>, Task>> middlewareRegistration;
+        private readonly MetricsContext context;
+        private readonly Func<HealthStatus> healthStatus;
+
+        private readonly bool isDisabled;
+
+        public AspNetCoreMetricsConfig(Action<Func<HttpContext, Func<Task>, Task>> middlewareRegistration, MetricsContext context, Func<HealthStatus> healthStatus)
+        {
+            this.middlewareRegistration = middlewareRegistration;
+            this.context = context;
+            this.healthStatus = healthStatus;
+        }
+
+        private AspNetCoreMetricsConfig()
+        {
+            this.isDisabled = true;
+        }
+
+        /// <summary>
+        /// Register all predefined AspNetCore metrics.
+        /// </summary>
+        /// <param name="ignoreRequestPathPatterns">Patterns for paths to ignore.</param>
+        /// <param name="aspNetCoreContext">Name of the metrics context where to register the metrics.</param>
+        /// <returns>Chainable configuration object.</returns>
+        public AspNetCoreMetricsConfig WithRequestMetricsConfig(Regex[] ignoreRequestPathPatterns = null, string aspNetCoreContext = "AspNetCore")
+        {
+            if (this.isDisabled)
+            {
+                return this;
+            }
+
+            return WithRequestMetricsConfig(config => config.WithAllAspNetCoreMetrics(), ignoreRequestPathPatterns, aspNetCoreContext);
+        }
+
+        /// <summary>
+        /// Configure which AspNetCore metrics to be registered.
+        /// </summary>
+        /// <param name="config">Action used to configure AspNetCore metrics.</param>
+        /// <param name="ignoreRequestPathPatterns">Patterns for paths to ignore.</param>
+        /// <param name="aspNetCoreContext">Name of the metrics context where to register the metrics.</param>
+        /// <returns>Chainable configuration object.</returns>
+        public AspNetCoreMetricsConfig WithRequestMetricsConfig(Action<AspNetCoreRequestMetricsConfig> config,
+            Regex[] ignoreRequestPathPatterns = null, string aspNetCoreContext = "AspNetCore")
+        {
+            if (this.isDisabled)
+            {
+                return this;
+            }
+
+            AspNetCoreRequestMetricsConfig requestConfig = new AspNetCoreRequestMetricsConfig(this.middlewareRegistration, this.context.Context(aspNetCoreContext), ignoreRequestPathPatterns);
+            config(requestConfig);
+            return this;
+        }
+
+        /// <summary>
+        /// Expose AspNetCore metrics endpoint
+        /// </summary>
+        /// <returns>Chainable configuration object.</returns>
+        public AspNetCoreMetricsConfig WithMetricsEndpoint()
+        {
+            if (this.isDisabled)
+            {
+                return this;
+            }
+
+            WithMetricsEndpoint(_ => { });
+            return this;
+        }
+
+        /// <summary>
+        /// Configure AspNetCore metrics endpoint.
+        /// </summary>
+        /// <param name="config">Action used to configure the Owin Metrics endpoint.</param>
+        /// <param name="endpointPrefix">The relative path the endpoint will be available at.</param>
+        /// <returns>Chainable configuration object.</returns>
+        public AspNetCoreMetricsConfig WithMetricsEndpoint(Action<MetricsEndpointReports> config, string endpointPrefix = "metrics")
+        {
+            if (this.isDisabled)
+            {
+                return this;
+            }
+
+            var endpointConfig = new MetricsEndpointReports(this.context.DataProvider, this.healthStatus);
+            config(endpointConfig);
+            
+            var metricsEndpointMiddleware = new AspNetCoreMetricsEndpointMiddleware(endpointPrefix, endpointConfig);
+            this.middlewareRegistration(metricsEndpointMiddleware.Invoke);
+            return this;
+        }
+    }
+}
---
+++ Src/Metrics.Tests/AspNetCoreAdapter/AspNetCoreMiddlewareTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AspNetCore.Metrics;
+using FluentAssertions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
+using AspNetCore.Metrics;
+using Xunit;
+
+namespace Metrics.Tests.AspNetCoreAdapter
+{
+    public class AspNetCoreMiddlewareTests
+    {
+        private const int timePerRequest = 100;
+
+        private readonly TestContext context = new TestContext();
+        private readonly MetricsConfig config;
+        private readonly TestServer server;
+
+        public AspNetCoreMiddlewareTests()
+        {
+            this.config = new MetricsConfig(this.context);
+
+            var builder = new WebHostBuilder()
+                .Configure(app =>
+                {
+                    this.config.WithAspNetCore(m => app.Use(m));
+
+                    app.Run(ctx =>
+                    {
+                        this.context.Clock.Advance(TimeUnit.Milliseconds, timePerRequest);
+                        if (ctx.Request.Path.ToString() == "/test/action")
+                        {
+                            return ctx.Response.WriteAsync("response");
+                        }
+
+                        if (ctx.Request.Path.ToString() == "/test/error")
+                        {
+                            ctx.Response.StatusCode = 500;
+                            return ctx.Response.WriteAsync("response");
+                        }
+
+                        if (ctx.Request.Path.ToString() == "/test/size")
+                        {
+                            return ctx.Response.WriteAsync("response");
+                        }
+
+                        if (ctx.Request.Path.ToString() == "/test/post")
+                        {
+                            return ctx.Response.WriteAsync("response");
+                        }
+
+                        ctx.Response.StatusCode = 404;
+                        return ctx.Response.WriteAsync("not found");
+                    });
+
+                });
+            
+            this.server = new TestServer(builder);
+        }
+
+
+        [Fact]
+        public async Task AspNetCoreMetrics_ShouldBeAbleToRecordErrors()
+        {
+            this.context.MeterValue("AspNetCore", "Errors").Count.Should().Be(0);
+            (await this.server.CreateRequest("http://local.test/test/error").GetAsync()).StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+            this.context.MeterValue("AspNetCore", "Errors").Count.Should().Be(1);
+
+            (await this.server.CreateRequest("http://local.test/test/error").GetAsync()).StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+            this.context.MeterValue("AspNetCore", "Errors").Count.Should().Be(2);
+        }
+
+        [Fact]
+        public async Task AspNetCoreMetrics_ShouldBeAbleToRecordActiveRequestCounts()
+        {
+            this.context.TimerValue("AspNetCore", "Requests").Rate.Count.Should().Be(0);
+            (await this.server.CreateRequest("http://local.test/test/action").GetAsync()).StatusCode.Should().Be(HttpStatusCode.OK);
+            this.context.TimerValue("AspNetCore", "Requests").Rate.Count.Should().Be(1);
+            (await this.server.CreateRequest("http://local.test/test/action").GetAsync()).StatusCode.Should().Be(HttpStatusCode.OK);
+            this.context.TimerValue("AspNetCore", "Requests").Rate.Count.Should().Be(2);
+            (await this.server.CreateRequest("http://local.test/test/action").GetAsync()).StatusCode.Should().Be(HttpStatusCode.OK);
+            this.context.TimerValue("AspNetCore", "Requests").Rate.Count.Should().Be(3);
+            (await this.server.CreateRequest("http://local.test/test/action").GetAsync()).StatusCode.Should().Be(HttpStatusCode.OK);
+            this.context.TimerValue("AspNetCore", "Requests").Rate.Count.Should().Be(4);
+
+            var timer = this.context.TimerValue("AspNetCore", "Requests");
+
+            timer.Histogram.Min.Should().Be(timePerRequest);
+            timer.Histogram.Max.Should().Be(timePerRequest);
+            timer.Histogram.Mean.Should().Be(timePerRequest);
+        }
+
+        [Fact]
+        public async Task AspNetCoreMetrics_ShouldRecordHistogramMetricsForPostSizeAndTimePerRequest()
+        {
+            const string json = "{ 'id': '1'} ";
+            var postContent = new StringContent(json);
+            postContent.Headers.Add("Content-Length", json.Length.ToString());
+            await this.server.CreateRequest("http://local.test/test/post").And(r => r.Content = postContent).PostAsync();
+
+            var histogram = this.context.HistogramValue("AspNetCore", "Post & Put Request Size");
+
+            histogram.Count.Should().Be(1);
+            histogram.LastValue.Should().Be(json.Length);
+        }
+    }
+}
